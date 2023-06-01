fa79d01dc8512371a3d521484d6d719c6fd0d69e
David Amin-Priddle <david.priddle2@gmail.com>
2023-06-01 13:32:05 +0100

    Fix up identity and allow user registration

--- OGame.MVC/Program.cs
+++ OGame.MVC/Program.cs
@@ -17,6 +17,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
