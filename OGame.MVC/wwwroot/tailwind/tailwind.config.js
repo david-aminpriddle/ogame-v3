66b77614b32b351df62ae200488f109692bd88e3
David Amin-Priddle <david.priddle2@gmail.com>
2023-06-01 14:52:54 +0100

    Buttons for getting people to the correct places

--- OGame.MVC/wwwroot/tailwind/tailwind.config.js
+++ OGame.MVC/wwwroot/tailwind/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["../../**/*.{cshtml,razor}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "ogame-blue": "#87CEEB",
+                "ogame-red": "#F08080",
+                "ogame-amber": "#EEE8AA",
+                "ogame-green": "#7FFFD4",
+                "ogame-primary": "#E68318",
+                "ogame-primary-darker": "#DE7E17",
+                "ogame-secondary": "#7B1E00",
+                "ogame-secondary-darker": "#661A00"
+            }
+        },
     },
     plugins: [],
 };
