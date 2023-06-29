f507332c66555cfd8c7317d9cb386f8b703e37ae
David Amin-Priddle <david.priddle2@gmail.com>
2023-06-29 10:19:48 +0100

    Start putting together a navigation system

--- OGame.MVC/Areas/Game/Controllers/HomeController.cs
+++ OGame.MVC/Areas/Game/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OGame.MVC.Areas.Game.Controllers;
